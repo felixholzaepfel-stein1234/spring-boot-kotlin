5606d63f05ea8eaef6e422ca85c82f4dac5076cc
fh <felix.holzaepfel-stein.ext.bayer.com>
2018-09-28 11:01:21 +0200

    Added a HelloController which returns hello directly

---
+++ helloservice/src/main/kotlin/de/fholzstein/helloservice/HelloController.kt
@@ -0,0 +1,11 @@
+package de.fholzstein.helloservice
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("hello")
+    fun hello () = "Hello"
+}
