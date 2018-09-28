f0b9bd3507dc91b055c71487592318eea212d474
fh <felix.holzaepfel-stein.ext.bayer.com>
2018-09-28 11:21:18 +0200

    integrated all three services

--- coroutines/src/main/kotlin/de/fholzstein/coroutines/HelloWorldController.kt
+++ coroutines/src/main/kotlin/de/fholzstein/coroutines/HelloWorldController.kt
@@ -1,11 +1,23 @@
 package de.fholzstein.coroutines
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
-class HelloWorldController {
+class HelloWorldController(@Autowired val restTemplateBuilder: RestTemplateBuilder) {
+    val restTemplate : RestTemplate
 
-    @GetMapping("/hello")
-    fun hello () = "Hello World"
+    init {
+        restTemplate = restTemplateBuilder.build()
+    }
+
+    @GetMapping("/helloWorld")
+    fun hello () : String {
+        val helloString = restTemplate.getForEntity("http://helloservice:8080/hello", String::class.java).body
+        val worldString = restTemplate.getForEntity("http://worldservice:8080/world", String::class.java).body
+        return "$helloString $worldString"
+    }
 }
