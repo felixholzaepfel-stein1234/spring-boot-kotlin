b0dcf93abf2fbf748727148207b8f36fe269fefb
Tom Vollerthun <tom.vollerthun@otto.de>
2018-09-28 15:18:34 +0200

    enable coroutines

--- coroutines/src/main/kotlin/de/fholzstein/coroutines/CoroutinesApplication.kt
+++ coroutines/src/main/kotlin/de/fholzstein/coroutines/CoroutinesApplication.kt
@@ -2,6 +2,12 @@ package de.fholzstein.coroutines
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.kotlin.experimental.coroutine.EnableCoroutine
+
+@Configuration
+@EnableCoroutine
+class Configuration
 
 @SpringBootApplication
 class CoroutinesApplication
--- coroutines/src/main/kotlin/de/fholzstein/coroutines/HelloWorldController.kt
+++ coroutines/src/main/kotlin/de/fholzstein/coroutines/HelloWorldController.kt
@@ -1,5 +1,9 @@
 package de.fholzstein.coroutines
 
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,13 +20,22 @@ class HelloWorldController(@Autowired val restTemplateBuilder: RestTemplateBuild
     }
 
     @GetMapping("/helloWorld")
-    fun hello(): String {
-        var helloString : String? = "something wrong"
-        var worldString : String? = "something wrong"
+    suspend fun hello(): String {
+        var helloDeferred : Deferred<String?>? = null
+        var worldDeferred : Deferred<String?>? = null
+
         val timeMillis = measureTimeMillis {
-            helloString = restTemplate.getForEntity("http://helloservice:8080/hello", String::class.java).body
-            worldString = restTemplate.getForEntity("http://worldservice:8080/world", String::class.java).body
+             launch {
+                helloDeferred = async {
+                    restTemplate.getForEntity("http://helloservice:8080/hello", String::class.java).body
+                }
+
+                worldDeferred = async {
+                    restTemplate.getForEntity("http://worldservice:8080/world", String::class.java).body
+                }
+            }.join()
         }
-        return "$helloString $worldString, called in $timeMillis ms"
+
+        return "${helloDeferred!!.await()} ${worldDeferred!!.await()}, called in $timeMillis ms"
     }
 }
